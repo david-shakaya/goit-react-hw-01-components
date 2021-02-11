efdf1eb7bd12774f0f57ea5f568ed33f08cfa3dc
david-shakaya <devidoz666@gmail.com>
2021-02-11 18:32:48 +0200

    + 1 Задан без стилей

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ProfileItem from './components/ProfileItem'
+import user from './user.json'
+
+const App =() => {
+  return <div>
+    <ProfileItem item={user}/>
+
+
+    
+  </div>
+
+}
+
+export default App
