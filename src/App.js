bb1958da49ba6eb605a8edd4b2d6440dc5727091
david-shakaya <devidoz666@gmail.com>
2021-02-11 20:41:35 +0200

    +

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ProfileItem from './components/ProfileItem'
-import user from './user.json'
+import React from "react";
+import ReactDOM from "react-dom";
+import ProfileItem from "./components/ProfileItem";
+import user from "./user.json";
 
-const App =() => {
-  return <div>
-    <ProfileItem item={user}/>
+const App = () => {
+  return (
+    <div>
+      <ProfileItem item={user} />
+    </div>
+  );
+};
 
-
-    
-  </div>
-
-}
-
-export default App
+export default App;
