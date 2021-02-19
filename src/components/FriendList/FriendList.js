6ae06be95cf527f66766070dc45e1e2a21605023
david-shakaya <devidoz666@gmail.com>
2021-02-19 13:03:56 +0200

    Зад 3

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -3,9 +3,14 @@ import FriendListItem from '../FriendListItem/FriendListItem';
 const FriendList = ({ friends }) => {
   return (
     <ul className="friend-list">
-      {friends.map(({ avatar, name, isOnline }) => {
+      {friends.map(({ avatar, name, isOnline, id }) => {
         return (
-          <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+          <FriendListItem
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+            key={id}
+          />
         );
       })}
     </ul>
--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import style from './FriendListItem.module.css';
 
 // FriendListItem avatar={avatar} name={name} isOnline={isOnline} />;
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <li className="item">
-      <span className="status">{isOnline}</span>
+    <li className={style.Item}>
+      {isOnline ? (
+        <span className={style.Online}></span>
+      ) : (
+        <span className={style.Ofline}> </span>
+      )}
       <img className="avatar" src={avatar} alt={name} width="48" />
-      <p className="name">{name}</p>
+      <p className={style.Text__item}>{name}</p>
     </li>
   );
 };
